7213bc975a9cbbda92d4f6854f8bfea753e97181
ningsammaa <126892932+ningsammaa@users.noreply.github.com>
2025-02-03 14:37:22 +0545

    Delete substring.py

--- substring.py
+++
@@ -1,7 +0,0 @@
-string = "What a wonderful world!"
-substring = "wonderful"
-
-if substring in string:
-    print(f"{substring} is found in the string.")
-else:
-    print(f"{substring} is not found in the string.")
