41573adf3226a0155985cf6093f26a43ea42a27a
ningsammaa <126892932+ningsammaa@users.noreply.github.com>
2025-02-06 16:01:27 +0545

    Add files via upload

---
+++ W1Ques1.py
@@ -0,0 +1,7 @@
+decimal_number = float(input("Enter a decimal number: "))
+integer_number = int(decimal_number)
+string_number = str(decimal_number)
+
+print(f"Origial float: {decimal_number}")
+print(f"Converted to integer: {integer_number}")
+print(f'Converted to string: "{string_number}"')
