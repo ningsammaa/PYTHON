e1a70a55cd0a84149edacfe53416228b09c85b5b
ningsammaa <126892932+ningsammaa@users.noreply.github.com>
2025-03-13 21:37:22 +0545

    Delete W1Ques2.py

--- W1Ques2.py
+++
@@ -1,6 +0,0 @@
-full_name = str(input("Enter full name: "))
-names = full_name.split()
-
-initial_one = names[0][0].upper()
-initial_two = names[-1][0].upper()
-print(f"Your initials are: {initial_one}.Nayom {initial_two}")
